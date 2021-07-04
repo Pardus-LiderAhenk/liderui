481c4e4fc73b726eb566eb09a55424c5516c461e
ismail BAŞARAN <basaran.ismaill@gmail.com>
2021-07-04 23:13:47 +0300

    vuex persistence api implemented.

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,15 @@
 import { createStore } from "vuex";
 import computerManagement from './modules/computerManagement';
+import VuexPersistence from 'vuex-persist';
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
   modules: {computerManagement,},
+  plugins:[vuexLocal.plugin]
 });
