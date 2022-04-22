984b081958031d028450add3da8f20d80ab167fa
ismail BAŞARAN <basaran.ismaill@gmail.com>
2022-04-22 14:03:31 +0300

    dashboardbox eklendi

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -1,15 +1,34 @@
 <template>
-    <h1>Merhaba Dashboard</h1>
+    <div>
+        <div class="p-grid">
+            <div class="p-col">
+                <Dashboardbox
+                title="Total Project Count"
+                description="Açıklama"
+                :icon="'pi pi-file-o'"
+                colorClass="blue"
+                />
+            </div>
+            <div class="p-col">
+                <Dashboardbox title="Total Procurement Count" description="Açıklama" colorClass="darkgray"/>
+            </div>
+            <div class="p-col">
+                <Dashboardbox title="Total Contract Count" description="Açıklama" :icon="'pi pi-star'" colorClass="orange"/>
+            </div>
+            <div class="p-col">
+                <Dashboardbox title="Total Contract Count" description="Açıklama" :icon="'pi pi-star'" colorClass="green"/>
+            </div>
+        </div>
+    </div>
  
 </template>
 
 
 <script>
+import Dashboardbox from "../../components/Dashboardbox/Dashboardbox.vue";
 
 export default {
-
-    components: {
-    }
+    components: { Dashboardbox },
 
 }
 </script>
