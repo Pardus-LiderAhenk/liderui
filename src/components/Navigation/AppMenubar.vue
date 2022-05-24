1f4a948cb6f84e6b19372c5dbf8650f9189ab79c
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2022-05-24 16:05:10 +0300

    app menubar fixed

--- src/components/Navigation/AppMenubar.vue
+++ src/components/Navigation/AppMenubar.vue
@@ -1,34 +1,36 @@
 <template>
-     <Menubar :model="items" style="background-color:#20639B; color:#fff">
-            <template #start>
-                 <router-link to="/">
-                        <img alt="Logo" src="../../assets/images/liderahenk-logo.png" style="width:140px" />
-                        
-                    </router-link>
-            </template>
-            <template #end>
-               
-                 <Button type="button" class=" p-button-link" @click="toggleLanguage">
-                    <img :src="getImgUrl($i18n.locale == 'tr' ? 'tr.png' : 'us.png')" style="width:20px;height:20px"/>
-                    <span class="pi pi-angle-down"></span>
-                </Button>
-                <Menu id="overlay_menu_lang" ref="languageMenu" :model="languages" :popup="true" />
-                <Button type="button" class=" p-button-link" @click="toggleSettings">
-                    <i class="pi pi-cog" style="fontSize: 1.2rem"></i>
-                    <span class="pi pi-angle-down"></span>
-                </Button>
-                <Menu id="overlay_menu" ref="settingsMenu" :model="settingItems" :popup="true" />
-                <Button type="button" class=" p-button-link" @click="toggleProfile">
-                    <i class="pi pi-user" style="fontSize: 1.2rem"></i>
-                    <span class="layout-topbar-icon pi pi-angle-down"></span>
-                </Button>
-                <Menu id="overlay_menu_profile" ref="profileMenu" :model="profileItems" :popup="true" />
-                
-            </template>
-        </Menubar>
+     <Menubar :model="menuItems" style="background-color:#20639B; color:#fff">
+        <template #start>
+            <router-link to="/">
+                <img alt="Logo" src="../../assets/images/liderahenk-logo.png" style="width:140px"/>
+            </router-link>
+        </template>
+        <template #end>
+            <Button type="button" class="p-button-link" @click="toggleLanguage">
+                <img :src="getImgUrl($i18n.locale == 'tr' ? 'tr.png' : 'us.png')" style="width:20px;height:20px"/>
+                <span class="pi pi-angle-down"></span>
+            </Button>
+            <Menu id="overlay_menu_lang" ref="languageMenu" :model="languages" :popup="true"/>
+            <Button type="button" class=" p-button-link" @click="toggleSettings">
+                <i class="pi pi-cog" style="fontSize: 1.2rem"></i>
+                <span class="pi pi-angle-down"></span>
+            </Button>
+            <Menu id="overlay_menu" ref="settingsMenu" :model="settingItemsMenu" :popup="true"/>
+            <Button type="button" class=" p-button-link" @click="toggleProfile">
+                <i class="pi pi-user" style="fontSize: 1.2rem"></i>
+                <span class="layout-topbar-icon pi pi-angle-down"></span>
+            </Button>
+            <Menu id="overlay_menu_profile" ref="profileMenu" :model="profileItemsMenu" :popup="true"/>
+        </template>
+    </Menubar>
 </template>
 
 <script>
+/**
+ * App Menubar
+ * @see {@link http://www.liderahenk.org/}
+ */
+ 
 import axios from 'axios';
 import { mapActions } from 'vuex';
 
@@ -40,21 +42,22 @@ export default {
                 {label:'TR', icon:'../../assets/images/flags/tr.png', command: () => {
                     this.$i18n.locale = 'tr';
                     this.updateUserLanguage('tr');
-                    this.$router.go();
+                    
                 }},
                 {label:'EN', icon:'../../assets/images/flags/us.png', command: () => {
                     this.$i18n.locale = 'en';
                     this.updateUserLanguage('en');
-                    this.$router.go();
                 }}
             ],
-            items: [ {
-                   label: this.$t('menu.computer_management'),
-                   to: '/computer'
+
+            items: [
+                {
+                    label: this.$t('menu.computer_management'),
+                    to: '/computer',
                 },
                 {
                     label: this.$t('menu.user_management'),
-                    to:'/user'
+                    to:'/user',
                 },
                 {
                     label: this.$t('menu.policy_management'),
@@ -81,64 +84,94 @@ export default {
                         }
                     ]
                 }
-            ],
-            settingItems: [
-                {
-					label: this.$t('menu.console_user_settings'),
-					to:'/settings/console_user_settings',
-				},
-				{
-					label: this.$t('menu.server_settings'),
-					to: '/settings/server_setings'
-				},
-                {
-					label: this.$t('menu.system_monitoring_definitions'),
-					to:'/settings/system_monitoring_definitions',
-				},
-                {
-					label: this.$t('menu.script_definitions'),
-					to: '/settings/script_definitions',
-				},
-                {
-					label: this.$t('menu.registration_templates'),
-					to:'/settings/registratin_templates',
-				},
-            ],
-            profileItems: [
-                {
-                    label: 'Profili Görüntüle',
-                    to: '/user_profile'
-                },
-                {
-                    label: 'Çıkış',
-                    command: () => {
-                        this.$store.dispatch("logout").then(() => this.$router.push("/login")).catch(err => console.log(err));
-                    }
-                }
             ]
         }
     },
+
+    computed: {
+        menuItems: {
+            get () {
+                return this.items;
+            },
+        },
+       
+        profileItemsMenu: {
+            get () {
+                let profileItems = [
+                    {
+                        label: this.$t('menu.view_profile'),
+                        to: '/user_profile',
+                        icon:'pi pi-user'
+                    },
+                    {
+                        label: this.$t('menu.logout'),
+                        icon:'pi pi-sign-out',
+                        command: () => {
+                            this.$store.dispatch("logout").then(() => this.$router.push("/login")).catch(err => console.log(err));
+                        }
+                    }
+                ]
+                return profileItems;
+            }
+        },
+
+        settingItemsMenu: {
+            get () {
+                let settingItems = [
+                    {
+                        label: this.$t('menu.console_user_settings'),
+                        to:'/settings/console_user_settings',
+                    },
+                    {
+                        label: this.$t('menu.server_settings'),
+                        to: '/settings/server_setings'
+                    },
+                    {
+                        label: this.$t('menu.system_monitoring_definitions'),
+                        to:'/settings/system_monitoring_definitions',
+                    },
+                    {
+                        label: this.$t('menu.script_definitions'),
+                        to: '/settings/script_definitions',
+                    },
+                    {
+                        label: this.$t('menu.registration_templates'),
+                        to:'/settings/registratin_templates',
+                    },
+                ]
+                return settingItems;
+            }
+        }
+    },
+
     methods: {
+
         ...mapActions([
             'updateUserLang',
         ]),
+        
         getImgUrl: function(path) {
             return require("../../assets/images/flags/" + path);
         },
+        
         toggleSettings(event) {
             this.$refs.settingsMenu.toggle(event);
         },
+        
         toggleProfile(event) {
             this.$refs.profileMenu.toggle(event);
         },
+
         toggleLanguage(event) {
             this.$refs.languageMenu.toggle(event);
         },
+
         updateUserLanguage(lang){
             let data = new FormData();
-            data.append('langName', lang);
-            axios.post('/changeLanguage', data);
-
+            data.append('preferredLanguage', lang);
+            axios.post('/changeLanguage' , data).then(response => {
+                this.$router.go();
+            });
             // UPDATE USER STATE
             this.$store.dispatch("updateUserLang", lang);
         }
