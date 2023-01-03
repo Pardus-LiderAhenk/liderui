2f9d77b1374459820d5426f69d059b3ba673be8a
3bru <16ebruarslan@gmail.com>
2023-01-03 11:44:34 +0300

    added browser chrome profile

---
+++ src/views/PolicyManagement/Tabs/Dialogs/Profiles/BrowserChrome/Tabs/PreferencesChrome.js
@@ -0,0 +1,52 @@
+/**
+ * Chrome global preferences
+ * @see {@link http://www.liderahenk.org/}
+ */
+
+
+export const PreferencesChrome = {
+    //		General preference
+            checkDefaultBrowser: "",
+            homePage: "",
+            pageMode: "",
+            useDownloadDir: "",
+            downloadDir: "",
+            useCustomDownloadDir: "",
+            openNewWindow: "",
+            warnOnClose: "",
+            warnOnOpen: "",
+            restoreOnDemand: "",
+            loadInBackground: "",
+            enableXpInstall: "",
+            //Proxy preference
+            proxyType: "",
+            httpProxy: "",
+            httpPort: "",
+            useThisServerForAllProtocols: "",
+            sslProxy: "",
+            sslPort: "",
+            ftpProxy: "",
+            ftpPort: "",
+            socksProxy: "",
+            socksPort: "",
+            socksVersion: "",
+            remoteDns: "",
+            autoProxyConfigUrl: "",
+            dontPromptForAuth: "",
+            noProxyOn: "",
+            //Privacy preference
+            dontWantToBeTracked: "",
+            rememberBrowsingDownloadHistory: "",
+            rememberSearchFormHistory: "",
+            acceptCookiesFromSites: "",
+            clearHistoryOnClose: "",
+            suggestHistory: "",
+            suggestBookmarks: "",
+            suggestOpenTabs: "",
+            keepCookiesUntil: "",
+
+}
+//taslarda command name göre ilerliyoruz
+//policy ileriliyoruz 
+
+export default PreferencesChrome;
